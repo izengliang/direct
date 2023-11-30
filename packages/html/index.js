bfad24253e89e006e9efe5f4ba470676cbcf8852
zale <lionzale@gmail.com>
2023-11-30 16:00:15 +0800

    Basic implementation code, concepts, and types.

--- packages/html/index.js
+++ packages/html/index.js
@@ -1,22 +1,241 @@
-const weakmap = new WeakMap();
+/**
+ *
+ * @param {Element} dom
+ * @returns {Comment[]}
+ */
+export const getAllComments = (dom) => {
+  /** @type {Comment[]} */
+  let comments = [];
+  const find = (dom) => {
+    dom.childNodes.forEach((n) => {
+      if (n.nodeType === Node.COMMENT_NODE) {
+        comments.push(n);
+      } else if (n.nodeType === Node.ELEMENT_NODE) {
+        find(n);
+      }
+    });
+  };
+  find(dom);
+  return comments;
+};
 
-const container = Symbol.for("#container");
+const templateCache = new Map();
 
 /**
- * template
+ * @enum {number}
  */
-class TemplateResult {
+export const ValueType = {
+  EVENT: 0,
+  PROPERTY: 1,
+  ATTRIBUTE: 2,
+  DIRECTIVE: 3,
+  TEMPLATE_RESULT: 4,
+};
+
+Object.freeze(ValueType);
+
+/**
+ * value information
+ *
+ * @typedef ValueInfo
+ * @property { Element } [host]
+ * @property { Comment } [marker] - When the position of value is the child of the host.
+ * @property { number } position - value index from values.
+ * @property { string } [propertyName]
+ * @property { string } [eventName]
+ * @property { string } [attributeName]
+ * @property { ValueType } type
+ *
+ */
+
+export class Template {
   /**
-   * @type {Element}
+   * @readonly
+   * @type {string[]}
    */
-  [container];
+  strings;
+  constructor(strings) {
+    this.strings = strings;
+    this.#mark();
+  }
+
+  #markedTemplateString;
+
+  /**
+   * mark value position info.
+   */
+  #mark() {
+    let template = "";
+
+    const strings = this.strings;
+    for (let i = 0; i < strings.length - 1; i++) {
+      const s = strings[i];
+
+      // is event handler
+      if (/\s*@(\w*)=\s*$/.test(s)) {
+        const eventName = RegExp.$1;
+        const s = RegExp["$`"];
+        template += s;
+        template += ` data-___marker-${i}='${JSON.stringify({
+          position: i,
+          eventName,
+          type: ValueType.EVENT,
+        })}'`;
+      } else if (/\s*\.(\w*)=\s*$/.test(s)) {
+        const s = RegExp["$`"];
+        const propertyName = RegExp.$1;
+        template += s;
+        template += ` data-___marker-${i}='${JSON.stringify({
+          position: i,
+          type: ValueType.PROPERTY,
+          propertyName,
+        })}'`;
+      } else if (/\s*(\w*)=\s*$/.test(s)) {
+        const attributeName = RegExp.$1;
+        const s = RegExp["$`"];
+        template += s;
+        template += ` data-___marker-${i}='${JSON.stringify({
+          index: i,
+          type: ValueType.ATTRIBUTE,
+          attributeName,
+        })}'`;
+      } else {
+        const beforeString = strings.slice(0, i).join("");
+        const tagIndex = beforeString.lastIndexOf("<");
+
+        if (
+          tagIndex !== -1 &&
+          /^<[a-z][A-Za-z0-9_\-]*(\s|.*\s)$/.test(beforeString.slice(tagIndex))
+        ) {
+          template += s;
+          template += ` data-___marker-${i}='${JSON.stringify({
+            index: i,
+            type: ValueType.DIRECTIVE,
+          })}'`;
+        } else {
+          template += s;
+          template += `<!--data-___marker-${i}=${JSON.stringify({
+            index: i,
+          })}-->`;
+        }
+      }
+    }
+    this.#markedTemplateString = template;
+  }
 
   /**
    * @type {DocumentFragment}
    */
-  fragment;
+  #templateFragment;
 
-  render() {}
+  createTemplateFragment() {
+    if (!this.#templateFragment) {
+      const template = document.createElement("template");
+      template.innerHTML = this.#markedTemplateString;
+      this.#templateFragment = template.content;
+    }
+  }
+
+  createTemplateResult() {
+    if (!this.#templateFragment) {
+      this.createTemplateFragment();
+    }
+    const valueInfos = [];
+    const templateFragment = this.#templateFragment.cloneNode(true);
+    const elements = templateFragment.querySelectorAll("*");
+    for (let n of elements) {
+      for (let mark in n.dataset) {
+        if (/^___marker\-\d+$/.test(mark)) {
+          valueInfos.push({
+            ...info,
+            host: n, // link host
+          });
+          delete n.dataset[mark];
+        }
+      }
+    }
+
+    const comments = getAllComments(templateFragment);
+    comments.forEach((comment) => {
+      const commentText = comment.textContent;
+      if (/^data-___marker\-\d+=(.*)/.test(commentText)) {
+        const info = JSON.parse(RegExp.$1);
+        valueInfos.push({
+          ...info,
+          marker: commentText,
+          host: n.parentNode,
+        });
+      }
+    });
+
+    return new TemplateResult(templateFragment, valueInfos);
+  }
+}
+
+/**
+ * template result
+ */
+class TemplateResult {
+  /**
+   * @type {DocumentFragment}
+   */
+  templateFragment;
+
+  /**
+   * @type {ValueInfo[]}
+   */
+  valueInfos;
+
+  /**
+   * @param {DocumentFragment} templateFragment;
+   * @param {ValueInfo[]} valueInfos;
+   */
+  constructor(templateFragment, valueInfos) {
+    this.templateFragment = templateFragment;
+    this.valueInfos = valueInfos;
+  }
+
+  #oldValues;
+
+  /**
+   * @param {any[]} values
+   */
+  render(values) {
+    for (let info of this.valueInfos) {
+      const value = values[info.position];
+      const oldValue = this.#oldValues[info.position];
+
+      switch (info.type) {
+        case ValueType.ATTRIBUTE:
+          {
+            info.host.setAttribute(info.attributeName, value);
+          }
+          break;
+        case ValueType.EVENT:
+          {
+            oldValue && info.host.removeEventListener(info.eventName, oldValue);
+            info.host.setAttribute(info.eventName, value);
+          }
+          break;
+        case ValueType.PROPERTY:
+          {
+            info.host[info.propertyName] = value;
+          }
+          break;
+
+        default:
+          // TODO
+          if (info.host) {
+            if (value instanceof Directive) {
+            }
+          } else if (info.marker) {
+            if (value instanceof Directive) {
+            } else if (value instanceof TemplateResult) {
+            }
+          }
+      }
+    }
+  }
 }
 
 /**
@@ -36,208 +255,4 @@ export const render = (template, containerElement, opt) => {
 class Directive {
   render(...values) {}
   update(part) {}
-}
-
-const map = new Map();
-
-/**
- * @typedef { { host: Element , event?:string, propname?:string, attrname?:string, index:number, type:"event"|"property"|"attribute",  value:any }[] } Value
- */
-
-/**
- *
- * @param {string[]} strings
- * @param  {...any} values
- */
-export function html(strings, ...values) {
-  const key = strings.join();
-
-  /**
-   * @type { { values: Value[], template: HTMLTemplateElement } }
-   */
-  let templateResult = map.get(key);
-
-  if (templateResult) {
-    /**
-     *  update values
-     */
-    const oldValues = templateResult.values;
-    for (let oldValue of oldValues) {
-      const newValue = values[oldValue.index];
-      if (newValue !== oldValue.value) {
-        switch (oldValue.type) {
-          case "attribute":
-            oldValue.host.setAttribute(oldValue.attrname, newValue);
-            break;
-
-          case "event":
-            oldValue.host.removeEventListener(oldValue.event, oldValue.value);
-            oldValue.host.addEventListener(oldValue.event, newValue);
-            break;
-
-          case "property":
-            oldValue.host.setAttribute(oldValue.propname, newValue);
-            break;
-        }
-      }
-    }
-  } else {
-    let template = "";
-
-    for (let i = 0; i < strings.length - 1; i++) {
-      const s = strings[i];
-      // is event handler
-      if (/\s*@(\w*)=\s*$/.test(s)) {
-        const eventname = RegExp.$1;
-        const s = RegExp["$`"];
-        template += s;
-        template += ` data-___marker-${i}='${JSON.stringify({
-          index: i,
-          type: "event",
-          event: eventname,
-        })}'`;
-      } else if (/\s*\.(\w*)=\s*$/.test(s)) {
-        const s = RegExp["$`"];
-        const propname = RegExp.$1;
-        template += s;
-        template += ` data-___marker-${i}='${JSON.stringify({
-          index: i,
-          type: "property",
-          propname: propname,
-        })}'`;
-      } else if (/\s*(\w*)=\s*$/.test(s)) {
-        const attrname = RegExp.$1;
-        const s = RegExp["$`"];
-        template += s;
-        template += ` data-___marker-${i}='${JSON.stringify({
-          index: i,
-          type: "attribute",
-          attrname: attrname,
-        })}'`;
-      } else {
-        template += s;
-        template += `data-___marker-${i}=''`;
-      }
-    }
-
-    template += strings.at(-1);
-
-    const t = document.createElement("template");
-    t.innerHTML = template;
-    /**
-     * @type {Value[]}
-     */
-    const values_ = [];
-    templateResult = { values: values_, template: t };
-    map.set(key, templateResult);
-
-    for (let n of t.content.children) {
-      const link = { node: n, values: {} };
-      for (let mark in n.dataset) {
-        if (/^___marker\-\d+$/.test(mark)) {
-          const info = JSON.parse(n.dataset[mark]);
-          switch (info.type) {
-            case "event":
-              const eventhandler = values[info.index];
-              n.addEventListener(info.event, eventhandler);
-              values_.push({
-                host: n,
-                event: info.event,
-                index: info.index,
-                type: "event",
-                value: eventhandler,
-              });
-              break;
-
-            case "property":
-              {
-                const v = (n[info.propname] = values[info.index]);
-                values_.push({
-                  host: n,
-                  propname: info.propname,
-                  index: info.index,
-                  type: "property",
-                  value: v,
-                });
-              }
-
-              break;
-
-            case "attribute":
-              {
-                const v = values[info.index];
-                n.setAttribute(info.attrname, v);
-                values_.push({
-                  host: n,
-                  attrname: info.attrname,
-                  index: info.index,
-                  type: "attribute",
-                  value: v,
-                });
-              }
-              break;
-
-            default: {
-              const v = values[info.index];
-
-              // Directive | string | other.toString()
-
-              if (v instanceof Directive) {
-                values_.push({
-                  host: n,
-                  index: info.index,
-                  type: "directive",
-                  value: v,
-                });
-              }
-            }
-          }
-          delete n.dataset[mark];
-        }
-      }
-    }
-
-    // child  directive link dom!
-    const dg = (node) => {
-      for (let n of node.childNodes) {
-        if (n.nodeType === Node.ELEMENT_NODE) {
-          dg(n);
-        } else if (n instanceof Text) {
-          const texts = n.textContent.split(/\s*(___marker\-\d+\=\'\')/);
-
-          for (let text of texts) {
-            if (/___marker\-(\d+)\=''/.test(text)) {
-              const i = RegExp.$1;
-              const value = values[i];
-              let part;
-              if (value instanceof Directive) {
-                part = new Comment();
-                n.parentNode.insertBefore(part, n);
-                part.values_.push({
-                  host: part,
-                  index: i,
-                  type: "directive",
-                  value: value,
-                });
-
-                const result = value.getValue();
-                if (typeof result !== "undefined") {
-                  console.log(result);
-                  //   n.parentNode.insertBefore(result, part);
-                }
-              } else {
-                part = new Text();
-                // n.parentNode.insertBefore(part, n);
-              }
-            } else {
-              //   n.parentNode.insertBefore(new Text(text), n);
-            }
-          }
-        }
-      }
-    };
-
-    dg(t.content);
-    return t.content;
-  }
 }
